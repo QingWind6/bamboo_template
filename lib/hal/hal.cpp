3453b26ace98200dd1aece407f75cc9c98e31232
JoJang2u <993628705@qq.com>
2026-01-29 15:29:57 +0800

    feat: implement HAL base class with dependency injection

--- lib/hal/hal.cpp
+++ lib/hal/hal.cpp
@@ -7,3 +7,93 @@
 
 #define LOG_TAG "HAL"
 
+using namespace HAL;
+
+static std::unique_ptr<HalBase> g_hal = nullptr;
+
+HalBase::HalBase()
+{
+}
+
+HalBase::~HalBase()
+{
+    SimpleLog::info(LOG_TAG, "HAL Destroyed");
+}
+
+// void HalBase::init()
+// {
+//     SimpleLog::log(LOG_TAG, "HalBase init");
+// }
+
+hal_components::BatteryBase& HalBase::GetBattery()
+{
+    if(!battery_)
+    {
+        SimpleLog::warn(LOG_TAG, "get battery null");
+        battery_ = std::make_unique<hal_components::BatteryBase>();
+    }
+    return *battery_;
+}
+
+hal_components::ButtonBase& HalBase::GetButton()
+{
+    if(!button_)
+    {
+        SimpleLog::warn(LOG_TAG, "get button null");
+        button_ = std::make_unique<hal_components::ButtonBase>();
+    }
+    return *button_;
+}
+
+hal_components::EpaperBase& HalBase::GetEpaper()
+{
+    if(!epaper_)
+    {
+        SimpleLog::warn(LOG_TAG, "get epaper null");
+        epaper_ = std::make_unique<hal_components::EpaperBase>();
+    }
+    return *epaper_;
+}
+
+hal_components::IndicatorBase& HalBase::GetIndicator()
+{
+    if(!indicator_)
+    {
+        SimpleLog::warn(LOG_TAG, "get indicator null");
+        indicator_ = std::make_unique<hal_components::IndicatorBase>();
+    }
+    return *indicator_;
+}
+
+void Inject(std::unique_ptr<HalBase> hal)
+{
+    if(hal)
+    {
+        Destroy();
+        g_hal = std::move(hal);
+        SimpleLog::log(LOG_TAG, "Inject HAL: %s", g_hal->type().c_str());
+        g_hal->init();
+    }else
+    {
+        SimpleLog::warn(LOG_TAG, "Attempt to inject nullptr HAL !!");
+    }
+}
+
+HalBase& Get()
+{
+    if(!g_hal)
+    {
+        SimpleLog::warn(LOG_TAG, "get HAL null");
+        Inject(std::make_unique<HalBase>());
+    }
+    return *g_hal;
+}
+
+void Destroy()
+{
+    if(g_hal)
+    {
+        SimpleLog::log(LOG_TAG, "Destroy HAL: %s", g_hal->type().c_str());
+        g_hal.reset();
+    }
+}
--- lib/hal/hal.h
+++ lib/hal/hal.h
@@ -13,17 +13,35 @@ namespace HAL{
 class HalBase{
 public:
     HalBase();
-    virtual ~HalBase() = default;
+    virtual ~HalBase();
+    // 返回当前 HAL 实现的类型名称，用于日志调试
+    virtual std::string type() { return "HalBase"; }
 
-    // Initialize the HAL and any registered components.
+    // 初始化 HAL 及其注册的组件
     virtual void init();
 
     hal_components::BatteryBase& GetBattery();
     hal_components::ButtonBase& GetButton();
     hal_components::EpaperBase& GetEpaper();
     hal_components::IndicatorBase& GetIndicator();
+protected:
+    // 组件指针，由派生类在 init() 中赋值
+    // 使用 unique_ptr 管理生命周期
+    std::unique_ptr<hal_components::BatteryBase> battery_;
+    std::unique_ptr<hal_components::ButtonBase> button_;
+    std::unique_ptr<hal_components::EpaperBase> epaper_;
+    std::unique_ptr<hal_components::IndicatorBase> indicator_;
 };
 
+// --- 全局入口 ---
+
+// 1. 获取全局 HAL 实例 (如果未注入，自动创建默认基类)
 HalBase& Get();
+
+// 2. 注入 HAL 实例
+void Inject(std::unique_ptr<HalBase> hal);
+
+// 3.销毁全局 HAL 实例
+void Destroy();
 
 }
