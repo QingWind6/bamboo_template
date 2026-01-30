9cff59cd08e09943b8e6c5a08e5f8518955dc3b0
JoJang2u <993628705@qq.com>
2026-01-30 16:20:00 +0800

    feat(app_display): add multi-button support and TFT display functionality

--- src/app/app_display.cpp
+++ src/app/app_display.cpp
@@ -5,6 +5,8 @@
 
 #include "hal/hal_esp32.h"
 
+#include "TFT_eSPI.h"
+
 AppDisplay::AppDisplay() { _app_name = "AppDisplay"; }
 
 
@@ -14,14 +16,23 @@ void AppDisplay::onSetup() {
 
 void AppDisplay::onRunning() {
     SimpleLog::info(getName(), "running");
-    static ButtonEvent::ButtonEvent_t button_id = ButtonEvent::None;
-    HAL::Get().GetButton().update();
-    button_id = HAL::Get().GetButton().get_event(ButtonId::ButtonId_t::KEY0);
-    if(button_id == ButtonEvent::click)
+    static ButtonEvent::ButtonEvent_t button0_id = ButtonEvent::None;
+    static ButtonEvent::ButtonEvent_t button1_id = ButtonEvent::None;
+    static ButtonEvent::ButtonEvent_t button2_id = ButtonEvent::None;
+    button0_id = HAL::Get().GetButton().get_event(ButtonId::ButtonId_t::KEY0);
+    button1_id = HAL::Get().GetButton().get_event(ButtonId::ButtonId_t::KEY1);
+    button2_id = HAL::Get().GetButton().get_event(ButtonId::ButtonId_t::KEY2);
+    if(button0_id == ButtonEvent::click)
     {
         SimpleLog::info(getName(), "Button KEY0 clicked, clearing display");
         HAL::Get().GetEpaper().clear();
+    }else if(button1_id == ButtonEvent::click)
+    {
+        SimpleLog::info(getName(), "Button KEY1 clicked, displaying black screen");
+        HAL::Get().GetEpaper().Get()->fillScreen(TFT_BLACK);
+        HAL::Get().GetEpaper().Get()->update();
     }
+
 }
 
 void AppDisplay::onDestroy() {
